c1ed2f955bfc2119827e3a24f8299575321a5116
Khemz-Developer <jalithapramod@gmail.com>
2024-06-07 02:32:48 +0530

    SignIn Page Design Finished

---
+++ components/LoginScreen.jsx
@@ -0,0 +1,55 @@
+import { View, Text,Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Colors } from '../constants/Colors'
+import { StyleSheet } from 'react-native'
+export default function LoginScreen() {
+  return (
+    <View>
+      <View style={{
+        display:'flex',
+        alignItems:'center',
+        marginTop:100
+      }}>
+      <Image style={{
+        width: 240,
+        height: 500,
+        borderRadius: 20,
+        borderWidth:4,
+        borderColor:'#000'
+       
+      
+      }} source={require('./../assets/images/login.png')} />
+      </View>
+
+      <View style={styles.style1}>
+        <Text style={{fontSize:30,fontFamily:'outfit-bold',textAlign:'center'}}>Your Ultimate
+          <Text style={{
+            color:'#7F57F1',
+          }}> Community Business Directory</Text> App    
+     
+        </Text>
+        <Text style={{fontSize:15, fontFamily:'outfit',marginVertical:20,textAlign:'center',color:'gray'}}>Find your favorite business near your and post your business to your community</Text>
+        <TouchableOpacity>
+           <Text style={styles.btn}>Let's Get Started !</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  style1:{
+    backgroundColor:'#fff',
+    padding:20,
+    marginTop:-20,
+    elevation:0
+  },
+  btn:{
+    backgroundColor: Colors.primary,
+    padding:10,
+    textAlign:'center',
+    width:200,
+    borderRadius:10,
+    color:'#fff',
+    alignSelf:'center',
+  }
+})
