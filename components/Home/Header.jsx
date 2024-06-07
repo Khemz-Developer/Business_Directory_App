c1dc9b446f8da00b6bbbe8bcf69e4262d395b342
Khemz-Developer <jalithapramod@gmail.com>
2024-06-07 18:03:35 +0530

    Frontend Slider Part Finished

---
+++ components/Home/Header.jsx
@@ -0,0 +1,62 @@
+import { View, Text , Image } from 'react-native'
+import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
+import { Colors } from '../../constants/Colors';
+import { Feather } from '@expo/vector-icons';
+import { TextInput } from 'react-native';
+export default function Header() {
+
+    const {user} = useUser();
+  
+    return (
+    <View style={{
+        padding:20,
+        paddingTop:40,
+        backgroundColor:Colors.primary,
+        borderBottomLeftRadius:20
+    }}>
+      <View style={{
+        display:'flex',
+        flexDirection:'row',
+        alignItems:'center',
+        gap:10
+        }}>
+        <Image style={{width:50,height:50, borderRadius:100}} source={{uri:user?.imageUrl}} />
+      
+      <View>
+        <Text style={{
+            color:'#fff'
+        }}>Welcome,</Text>
+        <Text style={{
+            fontSize:20,
+            fontFamily:'outfit-medium',
+            color:'#fff'
+        }}>{user?.fullName}</Text>
+      </View>
+
+      </View>
+
+        {/* Search Bar */}
+        <View style={{
+            display:"flex",
+            flexDirection:'row',
+            gap:10,
+            padding:10,
+            alignItems:'center',
+            backgroundColor:'#fff',
+            borderRadius:8,
+            marginTop:20
+
+        }}>
+        <Feather name="search" size={24} color={Colors.primary} />
+           <TextInput placeholder='Search... '
+           style={{
+            fontFamily:'outfit',
+            fontSize:16
+           }}
+           />
+        </View>
+
+    </View>
+  )
+}
---
+++ configs/FirebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFV8UT8uHTM99mUmDJQkVtL0MDCIxo3BQ",
+  authDomain: "business-directory-app-98c2f.firebaseapp.com",
+  projectId: "business-directory-app-98c2f",
+  storageBucket: "business-directory-app-98c2f.appspot.com",
+  messagingSenderId: "661420161553",
+  appId: "1:661420161553:web:45f8d3ae356d18c0e60fb0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
